c541ce6683c6fd277d168b4a742c548af1d271d1
machleslie <machariales@gmail.com>
2023-10-04 08:22:31 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+
+function submitData (userName , userEmail){
+    const data = {
+    name: userName,
+    email: userEmail,
+        
+    }
+    
+   const postObject = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-type":"application/json",
+            accept:"application/json"
+        }
+    
+    }
+    return fetch("http://localhost:3000/users" , postObject)
+    .then(function (res) {
+        return res.json();
+    })
+    .then(function(data) {
+        document.querySelector("body").textContent = data.id 
+    })
+    .catch((err) => {
+        document.querySelector("body").textContent = err.message;
+      });
+
+    }
+   
+submitData();
